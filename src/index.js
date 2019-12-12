a278a2f8069994292f465ddc3760916a3a3421d4
Makounouchi-Ippo <mehel-ka@student.42.fr>
2019-12-12 16:42:27 +0100

    video 288 udemy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter } from 'react-router-dom';
+
+const store = createStore();
 
 const app = (
-    <BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
         <App/>
     </BrowserRouter>
+    </Provider>
+    
 )
 
 ReactDOM.render(app, document.getElementById('root'));
