12ab7836ed4465e34a3d6486e1a5256db6681ad6
Mehdi EL KADDOURI <mehel-ka@e3r13p11.42.fr>
2020-02-01 18:46:44 +0100

    implementer les tags

--- src/container/Profil/Profil.js
+++ src/container/Profil/Profil.js
@@ -6,7 +6,8 @@ class Profil extends Component {
   state = {
       file : [],
       avatar: null,
-      disable: false
+      disable: false,
+      tags: null
   }
   handleChange = (e) => {
       let reader = new FileReader();
@@ -60,60 +61,149 @@ class Profil extends Component {
 
       return (
           <div  className={classes.backdrop_profil}>           
-            <div className={classes.photo}>  
-
-                    {avatar}  
-                    
-
+            <div className={classes.photo}> 
+                    {avatar}                   
                     <div  className={classes.buttons_group}>
                       <input style={{display:'none'}}type='file' onChange={this.handleChange}
                         ref={fileInput => this.fileInput = fileInput} /> 
                         <Button   onClick={() => this.fileInput.click()} disabled={disable}> Choose file</Button> 
                         <Button   variant="warning">Upload</Button>
                     </div>
-
                       {image}
-
                       <div className={classes.voir_profil}>
                             <a href="/profil">   <button className={classes.button}>Voir profil</button></a>
                       </div>
-                     
-            
             </div>
-
-
             <div className={classes.moitier}>
 
-
-                <div className={classes.moitier_gauche}>
-                   
+                <div className={classes.moitier_gauche}>  
                     <div className={classes.block_tags}>
                          <h4> Ce que j'aime... </h4>
                          <p style={{marginTop:'5px'}}>Choisissez dans la liste : </p>
                          <div className={classes.tags}>
-                            <form>
-                            <button type="checkbox" name="gender" value="male" checked> amour </button>
-                            </form>    
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Sport
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Humour
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Timide
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                             Musculation
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                             Culture
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Danse
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Natation
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Soiree entre amis
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Musique
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Voyage
+                          </div>
+                          <div class="form-col">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                            <label class="form-check-label" for="defaultCheck1"/>
+                              Mode
+                          </div>
                          </div>
-
                     </div>
-                  
                 </div>
 
                 <div className={classes.moitier_droite}>
-                     
-                     <div className={classes.form}>
-                          <div>  
-                             
+                  <div className={classes.form}>
+                     <form>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                              <label for="inputEmail4">Nom</label>
+                              <input   minLength="2" maxLength="15" required type="email" class="form-control" name="name" placeholder="Nom"/>
+                            </div>
+                            <div class="form-group col-md-6">
+                              <label for="inputPassword4">Prenom</label>
+                              <input minLength="2" maxLength="15" required type="password" class="form-control" id="lastname" placeholder="Prenom"/>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                              <label for="inputEmail4">mail</label>
+                              <input minLength="2" maxLength="15" required type="email" class="form-control" id="mail" placeholder="Mail"/>
+                            </div>
+                            <div class="form-group col-md-6">
+                              <label for="inputPassword4">Password</label>
+                              <input minLength="2" maxLength="15" required type="password" class="form-control" id="password" placeholder="Password"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="inputAddress">Address</label>
+                          <input minLength="2" maxLength="15" required type="text" class="form-control" id="login" placeholder="1234 Main St"/>
+                        </div>
+                        <div class="form-group">
+                          <label for="inputAddress2">Address 2</label>
+                          <input  required type="text" class="form-control" id="Address" placeholder="Apartment, studio, or floor"/>
+                        </div>
+                        <div class="form-row">
+                          <div class="form-group col-md-6">
+                            <label for="inputState">Orientation sexuel</label>
+                            <select id="inputState" class="form-control">
+                              <option selected>Choose...</option>
+                              <option>Hetero</option>
+                              <option>BI</option>
+                              <option>Gay</option>
+                            </select>
                           </div>
-
-
-
-                     </div>
+                          <div class="form-group col-md-6">
+                           <label  for="inputState">Genre</label>
+                            <select id="inputState" class="form-control">
+                              <option selected>Choose...</option>
+                              <option>Homme</option>
+                              <option>Femme</option>
+                              <option>Trans</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="gridCheck"/>
+                            <label class="form-check-label" for="gridCheck">
+                              Check me out
+                            </label>
+                          </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign in</button>
+                     </form>
+                  </div>
 
                 </div>
-
-
             </div>
           </div>
       )
