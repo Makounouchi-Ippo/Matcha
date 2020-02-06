cf9d5fd64907661d0e4d91e4871a7f4602f2c7ad
Mehdi EL KADDOURI <mehel-ka@e2r13p11.42.fr>
2020-02-06 16:50:24 +0100

    creation page profil partie droite pa fini

--- src/container/Profil/Profil.js
+++ src/container/Profil/Profil.js
@@ -1,54 +1,64 @@
 import React from 'react'
 import classes from './Profil.css'
+import { FiSend } from "react-icons/fi";
+import Progress from 'react-circle-progress-bar'
+
+
 
 const Profil = (props) => (
+    
     <div className={classes.Profil}>
         <div className={classes.gauche}>
             <div className={classes.description_gauche}> 
                             <div className={classes.identite}>
-                                <h5> mehdi el kaddouri</h5>
-                                <h6> Mehdi93400</h6>
-                                <h6> 50 ans, Paris 7e arrondissement </h6>
-                                <h6> Genre</h6>
+                                {/* <h5> mehdi el kaddouri</h5> */}
+                                <h1> Mehdi93400</h1>
+                                {/* <h6> 50 ans, Paris 7e arrondissement </h6> */}
+                                {/* <h6> Genre</h6> */}
                                 <h7> Connectée il y a moins de 24 heures</h7>
-                                <p>  Parisienne - Sports - Théâtre - Cinéma - Expos</p>
+                                {/* <p>  Parisienne - Sports - Théâtre - Cinéma - Expos</p> */}
 
                                 <p></p>
                             </div>   
-                            <div className={classes.photo}>
-                                            fgssgssssd
+                            <div className={classes.block_progress_bar}>
+                                <div >
+                                    <Progress progress={80}
+                                    
+                                        hideValue= {true}
+                                        subtitle={'Sccore de 78'}
+                                        strokeWidth={'15'}
+                                        reduction={'0.25'}
+                                        transitionDuration={'3.9'}
+                                        transitionTimingFunction={'ease'}
+                                        background={'#d5d5e8'}
+                                       
+                                        />
+                                </div> 
+                           </div>
+
+                            <div className={classes.block_photo}>
+                                    <div className={classes.photo}> 
+                                       Emplacement photo
+                                    </div>
                             </div>   
-                                <div>
-                            khgrgrgre
-                               re khgrgrgre
-                               regrgeredefefe
-                               eedddde khgrgrgre
-                               regrgeredefefe
-                               eedddde khgrgrgre
-                               regrgeredefefe
-                               eedddde khgrgrgre
-                               regrgeredefefe
-                               eedddde khgrgrgre
-                               regrgeredefefe
-                               eeddddegrgeredefefe
-                               eedddde
-                                </div>   
-                                <div>
-                            khgrgrgre
-                               re khgrgrgre
-                               regrgeredefefe
-                               eedddde khgrgrgre
-                               regrgeredefefe
-                               eedddde khgrgrgre
-                               regrgeredefefe
-                               eedddde khgrgrgre
-                               regrgeredefefe
-                               eedddde khgrgrgre
-                               regrgeredefefe
-                               eeddddegrgeredefefe
-                               eedddde
-                                </div>   
+                                <div className={classes.block_button}>
+                                <button> &#9825; Like me </button>    
+                                <button>   <FiSend>  </FiSend> Chattez  </button>  
+                            </div>   
             </div>
+        </div>
+        <div className= {classes.droite}>
+            <div className= {classes.gallery}>
+                <h5> Mes Photos </h5> 
+                <div className={classes.gallery_photo}>
+                    <div>ffff</div>
+                    <div>fff</div>
+                    <div>fff</div>
+                    <div>fff</div>
+                    <div>ffff</div>
+                </div>
+            </div>
+
 
         </div>
     </div>
